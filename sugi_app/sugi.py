51edb2f700be6cbbc7367e87d165e9c8e6446983
sugiuraakie <sugiuraakieakie@gmail.com>
2023-09-04 21:55:15 +0900

    fix

--- sugi_app/sugi.py
+++ sugi_app/sugi.py
@@ -10,7 +10,7 @@ import numpy as np
 classes = ["cat","dog"]
 image_size = 50
 
-UPLOAD_FOLDER = "uploads"
+UPLOAD_FOLDER = "sugi_app/uploads"
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
